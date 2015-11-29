37243375ef31a2cba036ce1ecabdb2d750ce169c
sohelrana820 <runwithlight820>
2015-11-30 00:21:08 +0600

    Signin and signup page design almost done

--- angular/app.js
+++ angular/app.js
@@ -1,32 +1,5 @@
-(function() {
-    var app = angular.module('bBundApp', ['ui.bootstrap', 'ngRoute']);
+var app = angular.module('bBundApp', ['ui.bootstrap', 'ngRoute']);
 
-    app.config(function($routeProvider) {
-        $routeProvider
+app.controller('HomeController', function(){
 
-            .when('/', {
-                templateUrl : 'templates/home.html',
-                controller  : 'HomeController'
-            })
-
-            .when('/users/login', {
-                templateUrl : 'templates/login.html',
-                controller  : 'UsersController'
-            })
-
-            .when('/users/signup', {
-                templateUrl : 'templates/signup.html',
-                controller  : 'UsersController'
-            })
-
-    });
-
-    app.controller('HomeController', function(){
-
-    });
-
-    app.controller('UsersController', function(){
-
-    });
-
-})();
+});
--- angular/routers.js
+++ angular/routers.js
@@ -2,3 +2,22 @@
  * Created by root on 11/29/15.
  */
 
+app.config(function($routeProvider) {
+    $routeProvider
+
+        .when('/', {
+            templateUrl : 'templates/home.html',
+            controller  : 'HomeController'
+        })
+
+        .when('/users/signin', {
+            templateUrl : 'templates/users/signin.html',
+            controller  : 'UsersController'
+        })
+
+        .when('/users/signup', {
+            templateUrl : 'templates/users/signup.html',
+            controller  : 'UsersController'
+        })
+
+});
